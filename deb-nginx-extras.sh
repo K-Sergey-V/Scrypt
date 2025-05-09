2d505f08220d41bcc7f50ea894fa78c42e49cff2
K-Sergey-V <109762208+K-Sergey-V@users.noreply.github.com>
2025-05-09 15:10:29 +0300

    Update deb-nginx-extras.sh

--- deb-nginx-extras.sh
+++ deb-nginx-extras.sh
@@ -3,7 +3,12 @@
 #---------------- Install NGINX (debian repository) ----------------
 #---Debian
 #---Install the prerequisites:
+echo "#============================================================"
+echo "update & upgrade:"
 sudo apt update & upgrade
+
+echo "#============================================================"
+echo "Install nginx-extras:"
 sudo apt install -y nginx nginx-extras
 
 echo "#============================================================"
