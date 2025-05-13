52a0cf422a238a14027e8d0a3bb9f5011dbb35e4
Sergey Krichko <s.v.krichko@gmail.com>
2025-05-13 16:21:17 +0300

    202505131621

--- deb-postgresql.sh
+++ deb-postgresql.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# wget https://raw.githubusercontent.com/K-Sergey-V/Scrypt/refs/heads/main/deb-postgresql.sh
+# Install lastet NGINX (nginx repository)
+# version by 2025-05-12-0027
+# Sergey Krichko
 #===================================================================================
 #---------------- Install POSTGRESQL (https://www.postgresql.org/) -----------------
 #---Debian
