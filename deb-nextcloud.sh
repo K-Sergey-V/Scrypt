316e4c6d46e7ef9181ccefd264f9cd5f13f763b4
Sergey Krichko <s.v.krichko@gmail.com>
2025-05-21 17:12:08 +0300

    nc+

--- deb-nextcloud.sh
+++ deb-nextcloud.sh
@@ -4,3 +4,52 @@
 # version by 2025-05-13-1558
 # Sergey Krichko
 
+===================================================================
+------------------------- install NEXTCLOUD -----------------------
+if [ -z "$1" ]; then
+    echo "Ошибка: не указана доменное имя облака"
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "Ошибка: не указана доменная зона облака"
+    exit 2
+fi
+nextclouddomainname=$1
+nextclouddomainzone=$2
+
+#---#---Подготовка сервера для установки NextCloud:
+#---#---дополнительный диск
+sudo mkfs.ext4 /dev/sdb
+sudo mkdir -p /extdata
+
+#---Просмотреть метку диска
+sudo blkid
+
+#---Добавить диск в загрузку
+sudo nano /etc/fstab
+UUID=e52f5d60-dcdd-4cc1-b65c-06526cc5e1ba /extdata  ext4  defaults  0 1
+
+#---Проверить монтирование раздела
+sudo mount -a
+df -h
+
+#---#---Сначала загрузим установщик сервера для коммунити редакции. Перейдите на сервер NextCloud и выполните загрузку дистрибутива:
+sudo wget -P /extdata/ https://download.nextcloud.com/server/releases/latest.tar.bz2
+
+#---Распакуйте архив:
+sudo tar -xjvf /extdata/latest.tar.bz2 -C /extdata/
+ls -la /extdata
+
+#---Создадим директорию для данных NextCloud сервера и скопирует туда все файлы дистрибутива:
+sudo mkdir -p /extdata/mylan.mycloud
+sudo cp -r /extdata/nextcloud/* /extdata/$nextclouddomainzone.$nextclouddomainname/
+
+#---Теперь необходимо изменить владельца для всего каталога /extdata/nextcloud:
+sudo chown -R nginx:nginx /extdata/$nextclouddomainzone.$nextclouddomainname
+sudo ls -la /extdata/
+#---#---Настройка NGINX
+#sudo nano /etc/nginx/sites-available/$nextclouddomainzone.$nextclouddomainname.conf
+#---#--- insert Server PARAM 
+
+
+
