80802c14ccf5caf4a54850d39eb1787a95648835
Sergey Krichko <s.v.krichko@gmail.com>
2025-05-22 15:12:49 +0300

    nc+tmp

--- deb-nextcloud.sh
+++ deb-nextcloud.sh
@@ -19,35 +19,35 @@ nextclouddomainzone=$2
 
 #---#---Подготовка сервера для установки NextCloud:
 #---#---дополнительный диск
-sudo mkfs.ext4 /dev/sdb
-sudo mkdir -p /extdata
+###sudo mkfs.ext4 /dev/sdb
+###sudo mkdir -p /extdata
 
 #---Просмотреть метку диска
-echo "\e[35m-----------------------------------------------------"
-sudo blkid
-sleep 3
+###echo "\e[35m-----------------------------------------------------"
+###sudo blkid
+###sleep 3
 #---Добавить диск в загрузку
-#sudo nano /etc/fstab
-#UUID=e52f5d60-dcdd-4cc1-b65c-06526cc5e1ba /extdata  ext4  defaults  0 1
+####sudo nano /etc/fstab
+###UUID=e52f5d60-dcdd-4cc1-b65c-06526cc5e1ba /extdata  ext4  defaults  0 1
 
 #---Проверить монтирование раздела
-sudo mount -a
-df -h
+###sudo mount -a
+###df -h
 
 #---#---Сначала загрузим установщик сервера для коммунити редакции. Перейдите на сервер NextCloud и выполните загрузку дистрибутива:
-sudo wget -P /extdata/ https://download.nextcloud.com/server/releases/latest.tar.bz2
+sudo wget -P /tmp/ https://download.nextcloud.com/server/releases/latest.tar.bz2
 
 #---Распакуйте архив:
-sudo tar -xjvf /extdata/latest.tar.bz2 -C /extdata/
-ls -la /extdata
+sudo tar -xjvf /tmp/latest.tar.bz2 -C /tmp/
+ls -la /tmp
 
 #---Создадим директорию для данных NextCloud сервера и скопирует туда все файлы дистрибутива:
-sudo mkdir -p /extdata/$nextclouddomainzone.$nextclouddomainname
-sudo cp -r /extdata/nextcloud/* /extdata/$nextclouddomainzone.$nextclouddomainname/
+sudo mkdir -p /tmp/$nextclouddomainzone.$nextclouddomainname
+sudo cp -r /tmp/nextcloud/* /tmp/$nextclouddomainzone.$nextclouddomainname/
 
 #---Теперь необходимо изменить владельца для всего каталога /extdata/nextcloud:
-sudo chown -R nginx:nginx /extdata/$nextclouddomainzone.$nextclouddomainname
-sudo ls -la /extdata/
+sudo chown -R nginx:nginx /tmp/$nextclouddomainzone.$nextclouddomainname
+sudo ls -la /tmp/
 #---#---Настройка NGINX
 #sudo nano /etc/nginx/sites-available/$nextclouddomainzone.$nextclouddomainname.conf
 #---#--- insert Server PARAM 
