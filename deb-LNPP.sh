b56dc13d703e1657038b6e1d7d27a692f7a18106
Sergey Krichko <s.v.krichko@gmail.com>
2025-06-10 20:20:45 +0300

    && bash

--- deb-LNPP.sh
+++ deb-LNPP.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# wget https://raw.githubusercontent.com/K-Sergey-V/Scrypt/refs/heads/main/deb-LNPP.sh
+# wget https://raw.githubusercontent.com/K-Sergey-V/Scrypt/refs/heads/main/deb-LNPP.sh && sudo bash deb-LNPP.sh
 # Install lastet NGINX (nginx repository)
 # version by 2025-05-13-1558
 # Sergey Krichko
